45d396760caf08dc48b2a5d0bcc485c2d8567d7a
Jerome Mac Lean <jerome@mac-lean.com>
2020-02-13 15:57:43 +0100

    Env by lib poc

--- src/com/daimler/openshift/OpenShiftEnvironment.groovy
+++ src/com/daimler/openshift/OpenShiftEnvironment.groovy
@@ -30,59 +30,14 @@ class OpenShiftEnvironment {
       this.prepTags = "${this.workspace_lib}/resources/com/cicd/jenkins/_confConvert.sh bv-1.00 1a2b3c4d"
 
 
-      context.echo "Environment: ${this.workspace}"
+      // context.echo "Environment: ${this.workspace}"
       this.prepTags.execute()
-      load ( "${this.workspace}/../workspace@libs/cicd-daimler-wltp-sim/resources/com/cicd/jenkins/env.files/generic.groovy".toString() )
       // load ( "${this.workspace}/../workspace@libs/cicd-daimler-wltp-sim/resources/com/cicd/jenkins/env.files/tag_env.groovy"toString() )
+  }
 
-
-
-
-
-
-      // this.appName = context.env.APP_NAME
-      // this.branchName = context.env.GIT_BRANCH
-      //     .replace('origin/', '')
-      //     .replace('/', '-')
-      //     .toLowerCase()
-      // this.s2iName = context.env.S2I
-      // this.s2iNamespace = context.env.S2I_NAMESPACE ?: 'ci'
-      // this.imageName = context.env.IMAGE_STREAM ?: this.appName
-      // this.imageNamespace = context.env.IMAGE_STREAM_NAMESPACE ?: 'ci'
-      // this.configMap = context.env.NAME + '-config'
-
-      // // For simpler cleanups, we'll label PR deployments as such
-      // if (context.env.CHANGE_ID) {
-      //     this.deployedFrom = 'pullRequest'
-      // } else if (this.branchName.equals('master')) {
-      //     this.deployedFrom = 'master'
-      // } else {
-      //     this.deployedFrom = 'branch'
-      // }
-
-      // // To leverage incremental builds, we need to have a stable tag.
-      // if (!this.branchName.equals('master')) {
-      //     this.tagName = this.branchName
-      // } else {
-      //     this.tagName = context.env.GIT_COMMIT.take(8)
-      // }
-
-      // this.internalService = (context.env.INTERNAL_SERVICE ?: 'false').equals('true');
-      // this.containerPort = context.env.CONTAINER_PORT ?: '8080'
-      // this.memoryLimit = context.env.MEMORY_LIMIT ?: '256Mi'
-      // this.memoryRequest = context.env.MEMORY_REQUEST ?: '128Mi'
-      // this.buildMemoryLimit  = context.env.BUILD_MEMORY_LIMIT ?: '1Gi'
-      // this.replicaCount = context.env.REPLICA_COUNT ?: '1'
-      // this.routerTimeout = context.env.ROUTER_TIMEOUT ?: '3m'
-
-      // this.buildConfig = context.libraryResource(
-      //     'com/daimler/openshift/BuildConfig.yml')
-
-      // this.deploymentConfig = context.libraryResource(
-      //     this.internalService
-      //         ? 'com/daimler/openshift/DeploymentConfigInternal.yml'
-      //         : 'com/daimler/openshift/DeploymentConfig.yml')
-
-      // context.echo "Environment initialized for ${this.appName}:${this.branchName}:${this.tagName}"
+  // --- Build Logic
+  def loadEnv() {
+    load ( "${this.workspace}/../workspace@libs/cicd-daimler-wltp-sim/resources/com/cicd/jenkins/env.files/generic.groovy".toString() )
+    return null
   }
 }

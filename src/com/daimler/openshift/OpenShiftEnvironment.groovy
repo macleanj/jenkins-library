b34d173efce29e02cd579cffc036bed2feb82c2a
Jerome Mac Lean <jerome@mac-lean.com>
2020-02-13 16:36:30 +0100

    Env by lib poc

--- src/com/daimler/openshift/OpenShiftEnvironment.groovy
+++ src/com/daimler/openshift/OpenShiftEnvironment.groovy
@@ -1,6 +1,8 @@
 
 package com.daimler.openshift
 
+import java.io.File 
+
 // ----------------------------------------------------
 // Environment Preperation Logic
 // * relies on the trigger by tag methodology
@@ -10,25 +12,31 @@ package com.daimler.openshift
 // ----------------------------------------------------
 
 class OpenShiftEnvironment {
-    // --- Resources
-    def workspace
-    def workspace_lib
-    // def envGeneric
+  // --- Resources
+  def workspace
+  def workspace_lib
+  // def envGeneric
 
-    // --- Data
-    def prepTags
+  // --- Data
+  def prepTags
 
   // --- Constructor
   OpenShiftEnvironment(context) {
-      this.workspace = context.env.WORKSPACE
-      this.workspace_lib = "${this.workspace}/../workspace@libs/cicd-daimler-wltp-sim"
-      // this.envGeneric = "${this.workspace_lib}/resources/com/cicd/jenkins/env.files/generic.groovy"
-      this.prepTags = "${this.workspace_lib}/resources/com/cicd/jenkins/_confConvert.sh bv-1.00 1a2b3c4d"
+    this.workspace = context.env.WORKSPACE
+    this.workspace_lib = "${this.workspace}/../workspace@libs/cicd-daimler-wltp-sim"
+    // this.envGeneric = "${this.workspace_lib}/resources/com/cicd/jenkins/env.files/generic.groovy"
+    this.prepTags = "${this.workspace_lib}/resources/com/cicd/jenkins/_confConvert.sh bv-1.00 1a2b3c4d"
 
 
-      // context.echo "Environment: ${this.workspace}"
-      this.prepTags.execute()
-      load "${this.workspace_lib}/resources/com/cicd/jenkins/env.files/generic.groovy"
-      // load ( "${this.workspace}/../workspace@libs/cicd-daimler-wltp-sim/resources/com/cicd/jenkins/env.files/tag_env.groovy"toString() )
+    // context.echo "Environment: ${this.workspace}"
+    this.prepTags.execute()
+
+    static void main(String[] args) { 
+      new File("${this.workspace_lib}/resources/com/cicd/jenkins/env.files/generic.groovy").eachLine {  
+        line -> println "line : $line"; 
+      } 
+    } 
+    // load "${this.workspace_lib}/resources/com/cicd/jenkins/env.files/generic.groovy"
+    // load ( "${this.workspace}/../workspace@libs/cicd-daimler-wltp-sim/resources/com/cicd/jenkins/env.files/tag_env.groovy"toString() )
+    }
   }
-}

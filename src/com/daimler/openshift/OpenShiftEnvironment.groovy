9692ef56dc0fc31f8a9d99897c16d71e6e068956
Jerome Mac Lean <jerome@mac-lean.com>
2020-02-13 12:21:33 +0100

    Env by lib poc

--- src/com/daimler/openshift/OpenShiftEnvironment.groovy
+++ src/com/daimler/openshift/OpenShiftEnvironment.groovy
@@ -25,7 +25,7 @@ class OpenShiftEnvironment {
   OpenShiftEnvironment(context) {
       this.context = context
       this.workspace = context.env.WORKSPACE
-      this.workspace_lib = "${this.workspace}/workspace\@libs"
+      this.workspace_lib = "${this.workspace}/workspace@libs"
       this.prepTags = "${this.workspace_lib}/resources/com/cicd/jenkins/_confConvert.sh bv-1.00 1a2b3c4d"
 
 

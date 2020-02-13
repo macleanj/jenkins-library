65f456aa189cd13bc35469af42e8f05901a37a1b
Jerome Mac Lean <jerome@mac-lean.com>
2020-02-13 16:26:19 +0100

    Env by lib poc

--- src/com/daimler/openshift/OpenShiftEnvironment.groovy
+++ src/com/daimler/openshift/OpenShiftEnvironment.groovy
@@ -13,7 +13,7 @@ class OpenShiftEnvironment {
     // --- Resources
     def workspace
     def workspace_lib
-    def envGeneric
+    // def envGeneric
 
     // --- Data
     def prepTags
@@ -22,18 +22,13 @@ class OpenShiftEnvironment {
   OpenShiftEnvironment(context) {
       this.workspace = context.env.WORKSPACE
       this.workspace_lib = "${this.workspace}/../workspace@libs/cicd-daimler-wltp-sim"
-      this.envGeneric = "${this.workspace_lib}/resources/com/cicd/jenkins/env.files/generic.groovy"
+      // this.envGeneric = "${this.workspace_lib}/resources/com/cicd/jenkins/env.files/generic.groovy"
       this.prepTags = "${this.workspace_lib}/resources/com/cicd/jenkins/_confConvert.sh bv-1.00 1a2b3c4d"
 
 
       // context.echo "Environment: ${this.workspace}"
       this.prepTags.execute()
+      load "${this.workspace_lib}/resources/com/cicd/jenkins/env.files/generic.groovy"
       // load ( "${this.workspace}/../workspace@libs/cicd-daimler-wltp-sim/resources/com/cicd/jenkins/env.files/tag_env.groovy"toString() )
-  }
-
-  // --- Build Logic
-  def loadEnv() {
-    load this.envGeneric
-    return null
   }
 }

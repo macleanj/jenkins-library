20d45dc5bbaa8e554c6a7d6f7d47565a693cd86d
Jerome Mac Lean <jerome@mac-lean.com>
2020-02-14 18:04:13 +0100

    bug fixing

--- src/com/cicd/jenkins/JenkinsEnvironment.groovy
+++ src/com/cicd/jenkins/JenkinsEnvironment.groovy
@@ -20,6 +20,10 @@ class JenkinsEnvironment {
 
   // --- Constructor
   JenkinsEnvironment(context) {
+    context.echo "Trigger by tag: git_commit ${context.env.GIT_COMMIT}"
+    context.echo "Trigger by tag: tag_name ${context.env.TAG_NAME}"
+    context.echo "Trigger by tag: change_id ${context.env.CHANGE_ID}"
+
     this.workspace = context.env.WORKSPACE
     this.workspace_lib = "${this.workspace}/../workspace@libs/cicd"
     this.prepTags = "${this.workspace_lib}/resources/com/cicd/jenkins/prepEnv.sh -git_commit ${context.env.GIT_COMMIT} -tag_name ${context.env.TAG_NAME} -change_id ${context.env.CHANGE_ID}"

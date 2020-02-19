a737b2f28c0a4fe78a29dd6ca892ff652dca069d
Jerome Mac Lean <jerome@mac-lean.com>
2020-02-19 16:30:16 +0100

    include WORKSPACE parsing

--- resources/com/cicd/jenkins/prepEnv.sh
+++ resources/com/cicd/jenkins/prepEnv.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # Main script to be triggered by the Jenkins pipeline
+# Required Environment:
+# - WORKSPACE: Workspace of the build (usually provided by Jenkins)
 
 programName=$(basename $0)
 programDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 baseName=$(echo ${programName} | sed -e 's/.sh//g')
 
-# cd "${1}" # Make sure the script will start in the WORKSPACE directory
-shift 1
 source $programDir/$baseName.conf
-# cd $programDir
 
 # Calculate tag directives
 $programDir/triggerByTag.sh "$@"

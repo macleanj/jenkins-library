f658f5d543e3440e09ea15188808b6e2c6df9a50
Jerome Mac Lean <jerome@mac-lean.com>
2020-02-19 16:09:36 +0100

    include WORKSPACE parsing

--- resources/com/cicd/jenkins/prepEnv.sh
+++ resources/com/cicd/jenkins/prepEnv.sh
@@ -5,8 +5,10 @@ programName=$(basename $0)
 programDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 baseName=$(echo ${programName} | sed -e 's/.sh//g')
 
+cd "${1}" # Make sure the script will start in the WORKSPACE directory
 shift 1
 source $programDir/$baseName.conf
+cd $programDir
 
 # Calculate tag directives
 $programDir/triggerByTag.sh "$@"

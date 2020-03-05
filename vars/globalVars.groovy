77160e47c087ed771f6a95991baa05702750d052
Jerome Mac Lean <jerome@mac-lean.com>
2020-03-05 11:17:22 +0100

    testing global vars

--- vars/globalVars.groovy
+++ vars/globalVars.groovy
@@ -1,8 +1,12 @@
-// package com.globalVars
-
-class globalVars {
-  final Integer buildThrottle = 1
-  // final String SUCCESS = "SUCCESS"
-  // final String FAILURE = "FAILURE"
-  // final String ABORTED = "ABORTED"
+def call() {
+  def globalVars = [:]
+  globalVars.buildThrottle = 1
+  return globalVars
 }
+
+// class globalVars {
+//   final Integer buildThrottle = 1
+//   // final String SUCCESS = "SUCCESS"
+//   // final String FAILURE = "FAILURE"
+//   // final String ABORTED = "ABORTED"
+// }

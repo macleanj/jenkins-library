924bec4427f7757e8ec6d6c0455bc8641652a164
Jerome Mac Lean <jerome@mac-lean.com>
2020-03-16 13:19:31 +0100

    testing file parse

--- vars/initCicd.groovy
+++ vars/initCicd.groovy
@@ -2,18 +2,21 @@ import com.cicd.jenkins.*
 
 def call() {
   // https://jenkins.io/doc/pipeline/steps/pipeline-utility-steps/#readyaml-read-yaml-from-files-in-the-workspace-or-text
-  def cicd = [build: [:], git: [:], jenkins: [:], config: [:], env: [:]]
 
-  cicd.build.debug = 1
-  cicd.build.throttle = 1
-  cicd.build.number = currentBuild.getNumber()
+  def cicd = CicdConfig().get()
 
-  node ('master') {
-    stage('Initialize CICD') {
-      sh 'echo "master - Stage: Initialize CICD"'
-      if (cicd.build.debug == 1) { echo "DEBUG: CICD Environment\n" + sh(script: "printenv | sort", returnStdout: true) }
-    }
-  }
+
+  // def cicd = [build: [:], git: [:], jenkins: [:], config: [:], env: [:]]
+  // cicd.build.debug = 1
+  // cicd.build.throttle = 1
+  // cicd.build.number = currentBuild.getNumber()
+
+  // node ('master') {
+  //   stage('Initialize CICD') {
+  //     sh 'echo "master - Stage: Initialize CICD"'
+  //     if (cicd.build.debug == 1) { echo "DEBUG: CICD Environment\n" + sh(script: "printenv | sort", returnStdout: true) }
+  //   }
+  // }
 
   return cicd
 }

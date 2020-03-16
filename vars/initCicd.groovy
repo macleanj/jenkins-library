a12498c7a192f3f8db36e11b31ba736517039772
Jerome Mac Lean <jerome@mac-lean.com>
2020-03-16 14:31:22 +0100

    testing file read

--- vars/initCicd.groovy
+++ vars/initCicd.groovy
@@ -2,21 +2,23 @@ import com.cicd.jenkins.CicdConfig
 
 def call() {
   // https://jenkins.io/doc/pipeline/steps/pipeline-utility-steps/#readyaml-read-yaml-from-files-in-the-workspace-or-text
+  // def cicdConfig = new CicdConfig()
+  // def cicd = cicdConfig.get()
+  // println cicd
 
   // def cicd = [build: [:], git: [:], jenkins: [:], config: [:], env: [:]]
   // cicd.build.debug = 1
   // cicd.build.throttle = 1
   // cicd.build.number = currentBuild.getNumber()
+  def props = libraryResource('com/cicd/jenkins/CicdConfig.yaml')
+  println props
 
-  node ('master') {
-    stage('Initialize CICD') {
-      sh 'echo "master - Stage: Initialize CICD"'
-      def cicdConfig = new CicdConfig()
-      def cicd = cicdConfig.get()
-      println cicd
-      if (cicd.build.debug == 1) { echo "DEBUG: CICD Environment\n" + sh(script: "printenv | sort", returnStdout: true) }
-    }
-  }
+  // node ('master') {
+  //   stage('Initialize CICD') {
+  //     sh 'echo "master - Stage: Initialize CICD"'
+  //     if (cicd.build.debug == 1) { echo "DEBUG: CICD Environment\n" + sh(script: "printenv | sort", returnStdout: true) }
+  //   }
+  // }
 
   return cicd
 }

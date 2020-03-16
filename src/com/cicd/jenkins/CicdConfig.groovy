e035d16cd70fbf500168b05cf48e22e57291c1c8
Jerome Mac Lean <jerome@mac-lean.com>
2020-03-16 13:58:30 +0100

    testing file read

--- src/com/cicd/jenkins/CicdConfig.groovy
+++ src/com/cicd/jenkins/CicdConfig.groovy
@@ -7,8 +7,9 @@ class CicdConfig {
   def get() {
     def filename = "com/cicd/jenkins/CicdConfig.yaml"
     println "DEBUG: loading filename: $filename"
+    def env_string = libraryResource "com/cicd/jenkins/CicdConfig.yaml"
     // def env_string = libraryResource filename
-    def env_string = sh(libraryResource(filename.toString()))
+    // def env_string = sh(libraryResource(filename.toString()))
     println "DEBUG: properties for build:\n$env_string"
 
     // Properties props = new Properties()

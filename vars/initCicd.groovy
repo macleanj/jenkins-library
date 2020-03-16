4e8b0118b44b2e3df86bb8023976261ddf797a2a
Jerome Mac Lean <jerome@mac-lean.com>
2020-03-16 12:26:30 +0100

    added pre-pipeline

--- vars/initCicd.groovy
+++ vars/initCicd.groovy
@@ -5,13 +5,25 @@ Defaults:
 
 */
 
-def call(Integer buildNumber) {
+def call() {
   def cicd = [build: [:], git: [:], jenkins: [:], config: [:], env: [:]]
   cicd.build.debug = 0
-  cicd.build.throttle = 100
+  cicd.build.throttle = 1
   cicd.build.number = currentBuild.getNumber()
+
   // cicd.config.appName = "hello-world"
   // cicd.jenkins.agentLabel = cicd.config.appName.toLowerCase().replaceAll("[_]", "-")
 
+pipeline {
+    agent { label 'master' }
+    stages {
+
+        stage('Build0') {
+            steps {
+               sh 'echo "hello world0" '
+            }  
+        }
+    }
+}
   return cicd
 }

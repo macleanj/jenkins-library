b284692e58c249c5411901faaa2700b4b852538c
Jerome Mac Lean <jerome@mac-lean.com>
2020-03-16 15:55:35 +0100

    local config modular

--- vars/cicdConfig.groovy
+++ vars/cicdConfig.groovy
@@ -1,22 +1,18 @@
 def call(project, service) {
-
-  // def props = libraryResource("com/cicd/${project}/${service}.yaml")
+  def debug = 1
 
   def filename = "com/cicd/${project}/${service}.yaml"
-  println "DEBUG: loading filename: $filename"
+  if (debug == 1) { println "DEBUG: loading filename: $filename" }
 
-  // Start getting yaml as (nested) object
+  // Getting yaml as (nested) object
   def objs = readYaml text: libraryResource(filename)
-  println "DEBUG: objects for build:\n$objs"
-  // END getting yaml as (nested) object
+  if (debug == 1) { println "DEBUG: objects for build:\n$objs" }
 
-  // Start getting properties
+  // Getting properties
   def env_string = libraryResource(filename)
-  println "DEBUG: properties for build:\n$env_string"
-
+  if (debug == 1) { println "DEBUG: properties for build:\n$env_string" }
   Properties props = new Properties()
   props.load(new ByteArrayInputStream(env_string.getBytes()))
-  // End getting properties
 
   return [objs, props]
 }

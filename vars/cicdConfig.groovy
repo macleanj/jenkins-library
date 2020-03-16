72eca610cc1671bf8e3306ed25d9bd9aaaf1f154
Jerome Mac Lean <jerome@mac-lean.com>
2020-03-16 15:01:56 +0100

    initial config

--- vars/cicdConfig.groovy
+++ vars/cicdConfig.groovy
@@ -1,13 +1,14 @@
 def call(project, service) {
 
-  def props = libraryResource("com/cicd/${project}/${service}.yaml")
+  // def props = libraryResource("com/cicd/${project}/${service}.yaml")
 
-  // def filename = "com/cicd/jenkins/CicdConfig.yaml"
-  // println "DEBUG: loading filename: $filename"
+  def filename = "com/cicd/${project}/${service}.yaml"
+  println "DEBUG: loading filename: $filename"
   // def env_string = libraryResource filename
-  // println "DEBUG: properties for build:\n$env_string"
+  def env_string = libraryResource(filename)
+  println "DEBUG: properties for build:\n$env_string"
 
-  // Properties props = new Properties()
-  // props.load(new ByteArrayInputStream(env_string.getBytes()))
+  Properties props = new Properties()
+  props.load(new ByteArrayInputStream(env_string.getBytes()))
   return props
 }

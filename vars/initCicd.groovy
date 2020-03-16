29fc97df6f001762fe665ef1bca5a7d88d450fa1
Jerome Mac Lean <jerome@mac-lean.com>
2020-03-16 09:29:26 +0100

    working on initCicd

--- vars/initCicd.groovy
+++ vars/initCicd.groovy
@@ -9,11 +9,9 @@ def call(Integer buildNumber) {
   def cicd = [build: [:], git: [:], jenkins: [:], config: [:], env: [:]]
   cicd.build.debug = 0
   cicd.build.throttle = 100
-  cicd.config.appName = "hello-world"
-  cicd.jenkins.agentLabel = cicd.config.appName.toLowerCase().replaceAll("[_]", "-")
-
-  println "My build: " + buildNumber
-  println "My build direct: " + currentBuild.getNumber()
+  cicd.build.number = currentBuild.getNumber()
+  // cicd.config.appName = "hello-world"
+  // cicd.jenkins.agentLabel = cicd.config.appName.toLowerCase().replaceAll("[_]", "-")
 
   return cicd
 }

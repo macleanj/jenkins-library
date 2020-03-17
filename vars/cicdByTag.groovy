99896d9ab3d30366f2606a8b6d5c7f11287f2725
Jerome Mac Lean <jerome@mac-lean.com>
2020-03-17 14:24:04 +0100

    Enhanced error handling

--- vars/cicdByTag.groovy
+++ vars/cicdByTag.groovy
@@ -23,6 +23,8 @@ def call() {
       cicdApp = readYaml file: 'config/AppConfig.yaml'
       cicd = mapMerge.merge(cicdGlobal, cicdApp)
       env.CICD_DEBUG = cicd.job.debug // Pass it to 'this' (context) in all methods to be able to enable global debug
+      echo "DEBUG class cicd.job.debug: " + cicd.job.debug.getClass()
+      echo "DEBUG class CICD_DEBUG: " + env.CICD_DEBUG.getClass()
       echo "DEBUG: " + env.CICD_DEBUG
 
       // Get git info, incl "trigger by tag" info

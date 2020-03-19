123d42321efcb00327280ed7abc39b94fcde1a6e
Jerome Mac Lean <jerome@mac-lean.com>
2020-03-19 16:00:15 +0100

    Optimizing lib

--- vars/cicdByTag.groovy
+++ vars/cicdByTag.groovy
@@ -44,7 +44,7 @@ def call() {
         // echo "gitCommitInfo\n" + prettyPrint(toJson(gitCommitInfo))
 
         GitUtils gitUtils = new GitUtils()
-        GithubReleaseInfo gitCommitInfo = gitUtils.getGithubRepoInfo(gitCommit, scm)
+        GithubRepoInfo gitCommitInfo = gitUtils.getGithubRepoInfo(gitCommit, scm)
         echo "gitCommitInfo\n" + prettyPrint(toJson(gitCommitInfo))
 
         // Enhance cicd config (object) with git info, incl "trigger by tag" info

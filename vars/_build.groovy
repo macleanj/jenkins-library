a384dcd0296a4fd8f86cd740aad7d1f6748d2424
Jerome Mac Lean <jerome@mac-lean.com>
2020-03-24 18:25:34 +0100

    Added dir for multi app pipeline

--- vars/_build.groovy
+++ vars/_build.groovy
@@ -1,9 +1,14 @@
 def call(context) {
   def cicd = context.cicd
   def log = context.log
+  def dir = "."
+
+  if (cicd.tag.appName) {
+    dir = "./" + cicd.tag.appName
+  }
 
   if (cicd.job.environment.container.buildingTool == 'img') {
     log.info("[" + cicd.job.environment.container.buildingTool + "] jenkins-builder - Build Image")
-    sh "img build -f ${cicd.job.environment.dockerFile} -t ${cicd.job.environment.registryUrl}/${cicd.job.environment.registrySpace}/${cicd.appName}:${cicd.tag.versionId} ."
+    sh "cd ${dir}; img build -f ${cicd.job.environment.dockerFile} -t ${cicd.job.environment.registryUrl}/${cicd.job.environment.registrySpace}/${cicd.appName}:${cicd.tag.versionId} ."
   }
 }

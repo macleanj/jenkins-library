50601a37d9c78a7742c95b7ef7f5f64a8a56c6c7
Jerome Mac Lean <jerome@mac-lean.com>
2020-03-25 13:43:51 +0100

    sizing

--- vars/_build.groovy
+++ vars/_build.groovy
@@ -7,8 +7,12 @@ def call(context) {
     dir = "./" + cicd.tag.appName
   }
 
+  def imageName = cicd.job.environment.registryUrl + "/" + cicd.job.environment.registrySpace + "/" + cicd.job.environment.appNamePrefix + cicd.appName + cicd.job.environment.appNamePostfix + ":" + cicd.tag.versionId
   if (cicd.job.environment.container.buildingTool == 'img') {
     log.info("[" + cicd.job.environment.container.buildingTool + "] jenkins-builder - Build Image")
-    sh "cd ${dir}; img build -f ${cicd.job.environment.dockerFile} -t ${cicd.job.environment.registryUrl}/${cicd.job.environment.registrySpace}/${cicd.appName}:${cicd.tag.versionId} ."
+    sh "cd ${dir}; img build -f ${cicd.job.environment.dockerFile} -t ${imageName} ."
   }
 }
+
+      
+      
--- vars/_publish.groovy
+++ vars/_publish.groovy
@@ -2,18 +2,21 @@ def call(context, user, pass) {
   def cicd = context.cicd
   def log = context.log
 
+  def imageName = cicd.job.environment.registryUrl + "/" + cicd.job.environment.registrySpace + "/" + cicd.job.environment.appNamePrefix + cicd.appName + cicd.job.environment.appNamePostfix + ":" + cicd.tag.versionId
+  def imageNameIs = cicd.job.environment.registryUrlLogin + "/" + cicd.job.environment.registrySpace + "/" + cicd.job.environment.appNamePrefix + cicd.appName + cicd.job.environment.appNamePostfix + ":" + cicd.tag.versionId
+
   if (cicd.job.environment.container.buildingTool == 'img') {
     if (cicd.job.environment.registryType == 'docker') {
       log.info("[" + cicd.job.environment.container.buildingTool + "] jenkins-builder - Publish Image - Docker Image Registry")
       sh "img login -u $user -p $pass ${cicd.job.environment.registryUrl}"
-      sh "img push ${cicd.job.environment.registryUrl}/${cicd.job.environment.registrySpace}/${cicd.appName}:${cicd.tag.versionId}"
+      sh "img push ${imageName}"
 
     } else if (cicd.job.environment.registryType == 'imagestream') {
       log.info("[" + cicd.job.environment.container.buildingTool + "] jenkins-builder - Publish Image - ImageStream Registry")
-      sh "img tag ${cicd.job.environment.registryUrl}/${cicd.job.environment.registrySpace}/${cicd.appName}:${cicd.tag.versionId} ${cicd.job.environment.registryUrlLogin}/${cicd.job.environment.registrySpace}/${cicd.appName}:${cicd.tag.versionId}"
+      sh "img tag ${imageName} ${imageNameIs}"
       sh "oc login https://${cicd.job.environment.registryUrlLogin} --token=$pass"
       sh "img login -u \$(oc whoami -t) -p \$(oc whoami -t) ${cicd.job.environment.registryUrlLogin}"
-      sh "img push ${cicd.job.environment.registryUrlLogin}/${cicd.job.environment.registrySpace}/${cicd.appName}:${cicd.tag.versionId}"
+      sh "img push ${imageNameIs}"
     }
   }
 }

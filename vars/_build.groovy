5b39587c04ae77aacff22fa4778b75b0bece0a95
Jerome Mac Lean <jerome@mac-lean.com>
2020-03-31 14:27:05 +0200

    Add registry credentials in build phase

--- vars/_build.groovy
+++ vars/_build.groovy
@@ -9,8 +9,16 @@ def call(context, user, pass) {
 
   def imageName = cicd.job.environment.registryUrl + "/" + cicd.job.environment.registrySpace + "/" + cicd.job.environment.appNamePrefix + cicd.appName + cicd.job.environment.appNamePostfix + ":" + cicd.tag.versionId
   if (cicd.job.environment.container.buildingTool == 'img') {
-    log.info("[" + cicd.job.environment.container.buildingTool + "] jenkins-builder - Build Image")
-    sh "img login -u $user -p $pass ${cicd.job.environment.registryUrl}"
-    sh "cd ${dir}; img build -f ${cicd.job.environment.dockerFile} -t ${imageName} ."
+    if (cicd.job.environment.registryType == 'docker') {
+      log.info("[" + cicd.job.environment.container.buildingTool + "] jenkins-builder - Build Image - Docker Image Registry")
+      sh "img login -u $user -p $pass ${cicd.job.environment.registryUrl}"
+      sh "cd ${dir}; img build -f ${cicd.job.environment.dockerFile} -t ${imageName} ."
+
+    } else if (cicd.job.environment.registryType == 'imagestream') {
+      log.info("[" + cicd.job.environment.container.buildingTool + "] jenkins-builder - Publish Image - ImageStream Registry")
+      sh "oc login https://${cicd.job.environment.registryUrlLogin} --token=$pass"
+      sh "img login -u \$(oc whoami -t) -p \$(oc whoami -t) ${cicd.job.environment.registryUrlLogin}"
+      sh "cd ${dir}; img build -f ${cicd.job.environment.dockerFile} -t ${imageName} ."
+    }
   }
 }

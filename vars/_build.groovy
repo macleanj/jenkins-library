e488e789d639a85dc26ebaf137382f9348b412be
Jerome Mac Lean <jerome@mac-lean.com>
2020-03-31 13:14:50 +0200

    Add registry credentials in build phase

--- vars/_build.groovy
+++ vars/_build.groovy
@@ -1,4 +1,4 @@
-def call(context) {
+def call(context, user, pass) {
   def cicd = context.cicd
   def log = context.log
   def dir = "."
@@ -10,9 +10,7 @@ def call(context) {
   def imageName = cicd.job.environment.registryUrl + "/" + cicd.job.environment.registrySpace + "/" + cicd.job.environment.appNamePrefix + cicd.appName + cicd.job.environment.appNamePostfix + ":" + cicd.tag.versionId
   if (cicd.job.environment.container.buildingTool == 'img') {
     log.info("[" + cicd.job.environment.container.buildingTool + "] jenkins-builder - Build Image")
+    sh "img login -u $user -p $pass ${cicd.job.environment.registryUrl}"
     sh "cd ${dir}; img build -f ${cicd.job.environment.dockerFile} -t ${imageName} ."
   }
 }
-
-      
-      
